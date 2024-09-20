80b3dffb3e80032fd3f6cd4384eacd04ce03bb95
Augustine Dickinson <augustine.dickinson@icloud.com>
2024-09-20 16:45:34 +0200

    Fix derived attributes for API

--- _plugins/json-api.rb
+++ _plugins/json-api.rb
@@ -1,35 +1,29 @@
 #require 'json'
 
-module JSONAPI
+Jekyll::Hooks.register :site, :pre_render do |site|
 
-  class Generator < Jekyll::Generator
-    def generate(site)
+  site.collections['works'].docs.each do |work|
 
-      site.collections['works'].docs.each do |work|
+    site.pages << Jekyll::PageWithoutAFile.new(site, site.source, 'api/json/works', "#{work.data['title']}.json").tap do |file|
 
-        site.pages << Jekyll::PageWithoutAFile.new(site, site.source, 'api/json/works', "#{work.data['title']}.json").tap do |file|
+      file.data.merge!(work.data)
+      file.data['layout'] = 'api/jsonapi'
 
-          file.data.merge!(work.data)
-          file.data['layout'] = 'api/jsonapi'
-
-          file.output
-
-        end
-
-      end
-
-      # subjects
-
-      # JSON.generate({
-      #   "type": "subjects",
-      #   "id": subject.data['id'],
-      #   "attributes": {
-      #     "type": subject.data['category']
-      #   },
-      #   relationships: { }
-      # })
+      file.output
 
     end
+
   end
+
+  # subjects
+
+  # JSON.generate({
+  #   "type": "subjects",
+  #   "id": subject.data['id'],
+  #   "attributes": {
+  #     "type": subject.data['category']
+  #   },
+  #   relationships: { }
+  # })
 
 end
--- _plugins/tei.rb
+++ _plugins/tei.rb
@@ -1,33 +1,27 @@
 require 'open-uri'
 
-module TEI
+Jekyll::Hooks.register :site, :pre_render do |site|
 
-  class Generator < Jekyll::Generator
-    def generate(site)
+  site.collections['works'].docs.each do |work|
 
-      site.collections['works'].docs.each do |work|
+    site.pages << Jekyll::PageWithoutAFile.new(site, site.source, 'api/xml/works', "#{work.data['title']}.xml").tap do |file|
 
-        site.pages << Jekyll::PageWithoutAFile.new(site, site.source, 'api/xml/works', "#{work.data['title']}.xml").tap do |file|
+      file.data.merge!(work.data)
+      file.data['layout'] = 'api/tei'
 
-          file.data.merge!(work.data)
-          file.data['layout'] = 'api/tei'
-
-          if work.data.include? 'zotero_key'
-            begin
-              raise "Skipping Zotero API call in development environment" if Jekyll::env == 'development'
-              file.data['bibliography'] = URI.open("https://api.zotero.org/groups/5599348/collections/#{work.data['zotero_key']}/items?v=3&format=tei").read.gsub!(/<\?.+\?>/, '')
-            rescue
-              file.data.delete('bibliography')
-            end
-          end
-
-          file.output
-
+      if work.data.include? 'zotero_key'
+        begin
+          raise "Skipping Zotero API call in development environment" if Jekyll::env == 'development'
+          file.data['bibliography'] = URI.open("https://api.zotero.org/groups/5599348/collections/#{work.data['zotero_key']}/items?v=3&format=tei").read.gsub!(/<\?.+\?>/, '')
+        rescue
+          file.data.delete('bibliography')
         end
-
       end
 
+      file.output
+
     end
+
   end
 
 end
